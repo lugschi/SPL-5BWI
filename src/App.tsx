11a44b4fe7fc683aca5a3fab7bc6836dfe24017d
Luca Grabherr <luca.grabherr@student.htldornbirn.at>
2024-11-12 12:01:55 +0100

    Cards created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import './App.css'
-import Card from './components/Card'
+import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <>
-      <Card />
-    </>
-  )
+    <div className="bg-black text-white min-h-screen p-8 flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-8">HTL Dornbirn 5bWI</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl">
+        <Card
+          image="https://picsum.photos/300/200?random=1"
+          name="Noel Riedmann"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=2"
+          name="Jonas Nigg"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=3"
+          name="Jonas Kronabitter"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=4"
+          name="Luca Grabherr"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=5"
+          name="Ian Felder"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=6"
+          name="Moritz Brandner"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=7"
+          name="Diego Zechner"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+        <Card
+          image="https://picsum.photos/300/200?random=8"
+          name="Ronny Kostmann"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,17 @@
+type Props = {
+  image: string;
+  name: string;
+  description: string;
+};
 
-type Props = {}
-
-export default function Card({}: Props) {
+export default function Card({ image, name, description }: Props) {
   return (
-    <div>Card</div>
-  )
+    <div className="bg-white text-black rounded-lg">
+      <img src={image} alt={name} className="w-full h-64 object-cover" />
+      <div className="p-4">
+        <h2 className="font-semibold text-lg">{name}</h2>
+        <p className="text-sm mt-2">{description}</p>
+      </div>
+    </div>
+  );
 }
